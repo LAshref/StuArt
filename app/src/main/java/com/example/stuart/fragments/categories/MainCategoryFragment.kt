850609b4977cf8190d62daaa2a2f1085a2a15091
LouayBks <louay.belkhamsa@supcom.tn>
2023-12-12 17:18:25 +0100

    fragment_main_category re-write

--- app/src/main/java/com/example/stuart/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/stuart/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,39 @@
 package com.example.stuart.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.stuart.R
+import com.example.stuart.adapters.SpecialProductsAdapter
+import com.example.stuart.databinding.FragmentMainCategoryBinding
 
 class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding: FragmentMainCategoryBinding
+    private lateinit var specialProductsAdapter: SpecialProductsAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupSpecialProductsRv()
+
+    }
+
+    private fun setupSpecialProductsRv(){
+        specialProductsAdapter = SpecialProductsAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL, false)
+            adapter = specialProductsAdapter
+
+        }
+    }
 }
