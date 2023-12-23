0488c2c7375ce05ce92571b15efc80da1b7564bc
gillaliglaou <mohamedhmaied92@gmail.com>
2023-12-23 08:47:13 +0100

    gkf

--- app/src/main/java/com/example/stuart/di/AppModule.kt
+++ app/src/main/java/com/example/stuart/di/AppModule.kt
@@ -3,9 +3,11 @@ package com.example.stuart.di
 import android.app.Application
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import com.example.stuart.firebase.FirebaseCommon
 import com.example.stuart.util.Constants.INTRODUCTION_SP
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
@@ -32,4 +34,11 @@ object AppModule {
     /* INTRODUCTION_SP is declared in the constants File
     MODE_PRIVATE : indicate that only our app can read from this function of  shared Preferences
      */
+    @Provides
+    @Singleton
+    fun provideFirebaseCommon(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore
+
+    )=FirebaseCommon(firestore, firebaseAuth)
 }
--- app/src/main/java/com/example/stuart/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/example/stuart/fragments/shopping/ProductDetailsFragment.kt
@@ -4,23 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.stuart.R
 import com.example.stuart.adapters.ColorsAdapter
 import com.example.stuart.adapters.SizesAdapter
 import com.example.stuart.adapters.ViewPager2Images
+import com.example.stuart.data.CartProduct
 import com.example.stuart.databinding.FragmentProductDetailsBinding
+import com.example.stuart.util.Resource
 import com.example.stuart.util.hideBottomNavigationView
+import com.example.stuart.viewmodel.Factory.DetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-
+@AndroidEntryPoint
 class ProductDetailsFragment: Fragment() {
     private val args by navArgs<ProductDetailsFragmentArgs>()
     private lateinit var binding : FragmentProductDetailsBinding
     private val viewPagerAdapter by lazy { ViewPager2Images() }
     private val sizesAdapter by lazy { SizesAdapter()}
     private val colorsAdapter by lazy { ColorsAdapter() }
+    private var selectedColor: Int? = null
+    private var selectedSize: String? = null
+    private val viewModel by viewModels<DetailsViewModel>()
 
 
     override fun onCreateView(
@@ -45,7 +57,33 @@ class ProductDetailsFragment: Fragment() {
         binding.imageClose.setOnClickListener{
             findNavController().navigateUp()
         }
-
+        sizesAdapter.onItemClick={
+            selectedSize=it
+        }
+        colorsAdapter.onItemClick={
+            selectedColor=it
+        }
+        binding.buttonAddtoCart.setOnClickListener {
+            viewModel.addUpdateProductInCart(CartProduct(product,1, selectedColor, selectedSize))
+        }
+        lifecycleScope.launchWhenStarted{
+            viewModel.addToCart.collectLatest {
+                when (it){
+                    is Resource.Loading ->{
+                        binding.buttonAddtoCart.startAnimation()
+                    }
+                    is Resource.Success ->{
+                        binding.buttonAddtoCart.revertAnimation()
+                        binding.buttonAddtoCart.setBackgroundColor((resources.getColor(R.color.black)))
+                    }
+                    is Resource.Error ->{
+                        binding.buttonAddtoCart.stopAnimation()
+                        Toast.makeText(requireContext(),it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
         binding.apply{
             tvProductName.text=product.name
             tvProductMPrice.text="${product.price}DT"
